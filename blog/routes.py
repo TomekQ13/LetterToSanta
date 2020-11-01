664b4ea1e4507273158028ac9a72fc0958d54618
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2020-11-01 23:10:12 +0100

    user authentication done

---
+++ blog/routes.py
@@ -0,0 +1,80 @@
+from flask import render_template, url_for, flash, redirect, request
+from blog import app, db, bcrypt
+from blog.forms import RegistrationForm, LoginForm
+from blog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+post = [
+    {
+        'author': 'Komsi Pomsi',
+        'title': 'KOCHAM JULSI',
+        'content': 'first post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Another Author',
+        'title': 'dummy post two',
+        'content': 'Second post content',
+        'date_posted': 'April 22, 2020'       
+    }
+
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = post)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created. You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()    
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
+
+    return render_template('login.html', title = 'Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title = 'Account')
