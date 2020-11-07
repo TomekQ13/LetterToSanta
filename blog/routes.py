ec3b020bbdf5225db57c9785293b0e099e65dad1
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2020-11-07 23:02:44 +0100

    pagination done

--- blog/routes.py
+++ blog/routes.py
@@ -13,7 +13,8 @@ from blog.models import Post, User
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.order_by(Post.date_posted.desc())
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts = posts)
 
 @app.route("/about")
@@ -140,7 +141,38 @@ def post_update(post_id):
         abort(403)
 
     form = PostForm()
-    form.title.data = post.title
-    form.content.data = post.content
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('The post has been updated.', 'success')
+        return redirect(url_for('post', post_id=post.id))
+
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
 
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def post_delete(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if post.author != current_user:
+        abort(403)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash('The post has been deleted.', 'success')
+
+    return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+
