cfafd6d087da749e231e66a8de878a819ccf1e2b
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2020-11-11 00:39:17 +0100

    no create all

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -1,13 +1,11 @@
 from flask import render_template, request, Blueprint
 from blog.models import Post
-from blog import db
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/home")
 def home():
-    db.create_all()
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts = posts)
--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from blog import create_app
-from blog.models import db
 
 app = create_app()
 
 if __name__ == '__main__':    
-    db.create_all()
     app.run(debug = True) 
 
